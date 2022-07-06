92dd064fb6e3a03dbf4e70f0cbed046e381a0a01
navetacandra <naveta.cand@gmail.com>
2022-07-06 14:52:59 +0700

    Restructure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-gsap.from("section > #about", { duration: 1, y: 60, opacity: 0.3 });
+gsap.from("section > #about", { duration: 0.6, y: 70, opacity: 0.3 });
 gsap
   .from("#profile-card", { duration: 1.4, rotationY: 105 })
   .to("#profile-card", { duration: 1.4, rotationY: 180 });
